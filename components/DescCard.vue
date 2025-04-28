38433bf215112b0d86e58fa13e4348b833b2fafa
henintsoapaul <henintsoapaul@gmail.com>
2025-04-28 23:15:38 +0300

    setup style for desc

--- components/DescCard.vue
+++ components/DescCard.vue
@@ -4,20 +4,29 @@ const props = defineProps({
   imgPath: {
     type: String,
     required: true
-  }
+  },
+  imgAlt: {
+    type: String,
+    required: true
+  },
 });
 
 </script>
 
 <template>
 <div id="desc-card">
-  <img :src="props.imgPath" alt="sofa">
+  <nuxt-img :src="props.imgPath" :alt="props.imgAlt"/>
 </div>
 </template>
 
 <style scoped lang="scss">
 #desc-card {
   background: #F9F1E7;
+  border-radius: 12px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
   img {
     width: 605px;
