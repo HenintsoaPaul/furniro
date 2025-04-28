ef1472a824f64eb1048ee7e9846f1ddd04223982
henintsoapaul <henintsoapaul@gmail.com>
2025-04-28 21:59:17 +0300

    update style for banner

--- components/Banner.vue
+++ components/Banner.vue
@@ -3,20 +3,23 @@
 </script>
 
 <template>
-  <div id="banner">
-    <nuxt-img src="/images/img.png" alt="banner img"/>
+  <section id="banner" aria-label="New Arrival banner">
+    <nuxt-img src="/images/img.png" alt="Scandinavian interior mockup with wall decal in cozy background" priority/>
 
     <!-- hover -->
-    <div>
-      <p class="sub">New Arrival</p>
-      <p class="maj">Discover Our
-        New Collection</p>
-      <p class="para">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper
-        mattis.</p>
+    <article>
+      <h5 class="sub">New Arrival</h5>
 
-      <button>BUY NOW</button>
-    </div>
-  </div>
+      <h2 class="maj">Discover Our New Collection</h2>
+
+      <h4 class="para">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper
+        mattis.</h4>
+
+      <div>
+        <nuxt-link to="/shop">BUY NOW</nuxt-link>
+      </div>
+    </article>
+  </section>
 </template>
 
 <style scoped lang="scss">
@@ -30,19 +33,19 @@
 
   position: relative;
 
-  div {
+  article {
     position: absolute;
     bottom: 20%;
-    right: 10%;
+    right: 8%;
     height: 28rem;
-    width: 40rem;
+    width: 42rem;
 
     background: #FFF3E3;
     padding: 5rem 2.5rem;
     box-sizing: border-box;
     border-radius: 6px;
 
-    p {
+    h2, h4, h5 {
       font-family: 'Poppins', sans-serif;
       margin: 0;
 
@@ -71,21 +74,21 @@
       }
     }
 
-    button {
-      margin-top: 3rem;
-      width: 35%;
-      height: 4rem;
-      border: none;
+    div {
+      margin-top: 5rem;
+      font-family: 'Poppins', sans-serif;
 
-      padding: 25px 72px ;
-
-      font-weight: 700;
-      font-size: 16px;
-      line-height: 100%;
-      letter-spacing: 3px;
-      color: #fff;
-
-      background-color: #B88E2F;
+      a {
+        text-decoration: none;
+        border: none;
+        padding: 25px 72px;
+        font-weight: 700;
+        font-size: 16px;
+        line-height: 100%;
+        letter-spacing: 3px;
+        color: #fff;
+        background-color: #B88E2F;
+      }
     }
   }
 }
