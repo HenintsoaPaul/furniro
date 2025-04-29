b39fd98e1812b88216db3897214809aac31c0d19
henintsoapaul <henintsoapaul@gmail.com>
2025-04-29 10:28:56 +0300

    browse: setup categories data

--- types/index.ts
+++ types/index.ts
@@ -8,3 +8,13 @@ export interface IconLink {
     icon: object;
     label: string;
 }
+
+export interface ImgData {
+    src: string;
+    alt: string;
+}
+
+export interface BrowseCard {
+    imgData: ImgData,
+    title: string,
+}
