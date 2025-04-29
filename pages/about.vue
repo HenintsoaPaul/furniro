52f5bfd9ce9cc70c0ea87f6465b11ac0ae18b21d
henintsoapaul <henintsoapaul@gmail.com>
2025-04-29 20:53:05 +0300

    add pages mockup for redirection

---
+++ pages/about.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+useHead({
+  link: [
+    {
+      rel: 'stylesheet',
+      href: 'https://fonts.googleapis.com/css2?family=Poppins&family=Montserrat&display=swap'
+    }
+  ]
+})
+</script>
+
+<template>
+  <div id="about">
+    <Header />
+
+    <div>
+      <h1>About</h1>
+    </div>
+
+    <Footer />
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
